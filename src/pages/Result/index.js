493ec42a0fc1505fae14ea3af7d71b39e3ca0bad
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-07-14 15:37:25 -0300

    Informações do perfil ok

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -13,19 +13,31 @@ class Result extends Component {
 
   componentDidMount = async () => {
     console.log("O componente foi montado")
-    const { location } = this.props;  //PRECISAAAA COLOCAR ISSO, OU VAI QUEBRAR
-    await this.setState({ user: this.props.history.location.state });
-    console.log(this.props.history.location.state)
+    const { location } = this.props; //PRECISAAAA COLOCAR ISSO, OU VAI QUEBRAR
+    const user = location.state.res; 
+    console.log(user)
+    await this.setState({ user });
     console.log(this.state.user)
+    console.log(this.props.history.location.state)
   };
 
   render() {
     const { user } = this.state;
-    console.log(user) //printa {}
+    console.log(user.avatar_url) //printa {}    
     return (
       <main >
         <div className="content">
-          <h1>OI </h1>
+          <h1>{user.name} </h1>
+
+          <div className="perfil__container">
+            <img src={user.avatar_url} alt="foto de perfil"></img>
+            <h3>{user.name} </h3>           
+            <p>{user.bio}</p>
+            <p>{user.location}</p>
+            <p>{user.public_repos}</p>
+            <p>{user.followers}</p>
+            <p>{user.following}</p>      
+          </div>
         </div>
       </main>
     );
